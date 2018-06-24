038bafe9d1eb90861ccc22e3a036991cd5715cee
afeiship <1290657123@qq.com>
2018-06-24 18:22:00 +0800

    issue: fix path

--- index.sh
+++ index.sh
@@ -4,6 +4,6 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 export PUB_HOSTED_URL=https://pub.flutter-io.cn;
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn;
-export PATH=~/github/flutter/flutter/bin:$PATH;
+export PATH=~/github/flutter/bin:$PATH;
 
 unset ROOT_PATH;
