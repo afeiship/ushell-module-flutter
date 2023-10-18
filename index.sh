bece011d5c3ad3e38faa6ff98ba25db7089ba7ab
afeiship <1290657123@qq.com>
2023-10-18 21:19:34 +0800

    feat: add dart cli

--- index.sh
+++ index.sh
@@ -11,4 +11,7 @@ export PUB_HOSTED_URL=https://pub.flutter-io.cn;
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn;
 
 
+# dart-cli
+export PATH="$PATH":"$HOME/.pub-cache/bin";
+
 unset ROOT_PATH;
